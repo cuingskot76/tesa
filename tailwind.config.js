329be6a67fbb62aa3b41c3587c38f3c9b85243c5
cuingskot76 <afrizapamujo@gmail.com>
2024-02-02 22:10:59 +0700

    chore(theme): add main colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
         primary: "#E75D8F",
         primary_50: "#E75D8F4D",
         primary_35: "#E75D8F59",
-        white: "#FFF5F9"
+        white: "#FFF5F9",
+        gray: "#D9D9D9",
+        yellow: "#FFBD59",
+        blue: "#004AAD",
+        red_dark: "#56091F",
+        green: "#2AB85B"
       }
     }
   },
