7766c56c1e04444bdcb201cbfc416ee23d805b98
cuingskot76 <afrizapamujo@gmail.com>
2024-02-05 21:31:01 +0700

    feat(data): add dumy menu data

---
+++ constant/index.ts
@@ -0,0 +1,83 @@
+export const menu = [
+  {
+    id: "1",
+    name: "Pecel Sayur",
+    ingredients:
+      "1 ikat bayam, ambil daunnya, siangi 5 lembar kol, iris sesuai selera 6 lonjor kacang panjang, potong sekitar 3 cm 50 gram taoge, siangi 150 ml air hangat",
+    spices:
+      "250 gram kacang tanah, goreng matang 3 buah cabai merah besar 8 buah cabai rawit merah 3 siung bawang putih 5 cm kencur 3 sdm gula merah, sisir 1 sdt garam 1 sdm air asam jawa",
+    steps:
+      "Bumbu pecel: Goreng cabai merah besar, cabai rawit merah, dan bawang putih hingga layu. Haluskan kacang tanah goreng bersama tumisan cabai, kencur, gula merah, dan garam. Tambahkan air asam jawa dan air hangat. Aduk hingga tercampur rata. Rebus daun bayam, irisan kol, potongan kacang panjang, dan taoge hingga matang, tiriskan. Siapkan pinggan, tata sayuran di dalam pinggan. Tuang dengan bumbu pecel di atasnya. Sajikan pecel sayur dengan rempeyek udang.",
+    image: require("../assets/foods/1.png")
+  },
+  {
+    id: "2",
+    name: "Udan Brokoli",
+    ingredients: "1 batang brokoli 7-8 buah udang (sesuai selera)",
+    spices:
+      "2 buah bawang putih 3 cabai rawit daun bawang air (sedikit saja) minyak 1/2 sdt gula 1 sdt kaldu jamur (bisa diganti kecap asin/garam) secukupnya lada",
+    steps:
+      "Potong brokoli ambil bagian daunnya, lalu cuci bersih dan rendam di air garam kurleb 3-5 menit.selanjutnya rebus brokoli sampai dirasa lumayan empuk sekitar 5 menit (ditusuk pakai garpu jika dirasa lumayan empuk matikan kompor) Langkah 2 Cuci bersih udang, buang kepalanya saja. Langkah 3 Siapkan wajan kasih sedikit minyak, lalu tumis bumbu2.masukkan udang dan tumis sebentar. setelah itu masukkan air sedikit saja Langkah 4 Masukkan brokoli yg sudah direbus tadi, lalu tumis sampai matang. Tumis Brokoli Udang siap ,dinikmati.",
+    image: require("../assets/foods/2.png")
+  },
+  {
+    id: "3",
+    name: "Ayam Bakar",
+    ingredients: "1 ekor ayam, potong 4 bagian 2 sdm kecap manis",
+    spices:
+      "5 siung bawang merah 3 siung bawang putih 1 sdt ketumbar 1/2 sdt merica 1/2 sdt garam 1 sdm gula pasir 1 sdm air asam jawa 2 sdm minyak goreng",
+    steps:
+      "Haluskan bawang merah, bawang putih, ketumbar, merica, garam, dan gula pasir. Tambahkan air asam jawa dan minyak goreng. Aduk rata. Lumuri ayam dengan bumbu halus dan kecap manis. Diamkan selama 30 menit. Bakar ayam di atas bara api hingga matang dan harum. Sajikan ayam bakar dengan sambal kecap dan lalapan.",
+    image: require("../assets/foods/3.png")
+  },
+  {
+    id: "4",
+    name: "Sate Ayam",
+    ingredients: "500 gram daging ayam, potong dadu 1 cm",
+    spices:
+      "5 siung bawang putih 1 sdt ketumbar 1/2 sdt merica 1 sdt garam 1 sdm gula pasir 2 sdm kecap manis 2 sdm minyak goreng",
+    steps:
+      "Haluskan bawang putih, ketumbar, merica, garam, dan gula pasir. Tambahkan kecap manis dan minyak goreng. Aduk rata. Lumuri daging ayam dengan bumbu halus. Diamkan selama 30 menit. Tusuk daging ayam di tusukan sate. Bakar di atas bara api hingga matang dan harum. Sajikan sate ayam dengan bumbu kacang dan lontong.",
+    image: require("../assets/foods/4.png")
+  },
+  {
+    id: "5",
+    name: "Nasi Goreng",
+    ingredients: "300 gram nasi putih 100 gram daging ayam, potong dadu",
+    spices:
+      "3 siung bawang putih 2 butir telur 1 sdm kecap manis 1 sdm saus tiram 1/2 sdt garam 1/4 sdt merica 1 batang daun bawang, iris halus 2 sdm minyak goreng",
+    steps:
+      "Tumis bawang putih hingga harum. Masukkan daging ayam. Aduk hingga berubah warna. Masukkan telur. Aduk hingga berbutir. Tambahkan nasi putih, kecap manis, saus tiram, garam, dan merica. Aduk hingga tercampur rata. Masukkan daun bawang. Aduk hingga matang. Sajikan nasi goreng dengan irisan mentimun dan kerupuk.",
+    image: require("../assets/foods/5.png")
+  },
+  {
+    id: "6",
+    name: "Mie Goreng",
+    ingredients: "200 gram mie telur 100 gram daging ayam, potong dadu",
+    spices:
+      "3 siung bawang putih 2 butir telur 1 sdm kecap manis 1 sdm saus tiram 1/2 sdt garam 1/4 sdt merica 1 batang daun bawang, iris halus 2 sdm minyak goreng",
+    steps:
+      "Rebus mie telur hingga matang. Tiriskan. Tumis bawang putih hingga harum. Masukkan daging ayam. Aduk hingga berubah warna. Masukkan telur. Aduk hingga berbutir. Tambahkan mie telur, kecap manis, saus tiram, garam, dan merica. Aduk hingga tercampur rata. Masukkan daun bawang. Aduk hingga matang. Sajikan mie goreng dengan irisan mentimun dan kerupuk.",
+    image: require("../assets/foods/6.png")
+  },
+  {
+    id: "7",
+    name: "Soto Ayam",
+    ingredients: "1 ekor ayam, potong 4 bagian 1 liter air",
+    spices:
+      "5 siung bawang putih 3 siung bawang merah 1 sdt ketumbar 1/2 sdt merica 1 sdt garam 1 sdm gula pasir 2 sdm minyak goreng",
+    steps:
+      "Rebus ayam hingga matang. Angkat dan suwir-suwir dagingnya. Saring kaldu ayam. Haluskan bawang putih, bawang merah, ketumbar, merica, garam, dan gula pasir. Tumis bumbu halus hingga harum. Masukkan kaldu ayam. Aduk rata. Masukkan suwiran daging ayam. Aduk rata. Sajikan soto ayam dengan pelengkapnya.",
+    image: require("../assets/foods/7.png")
+  },
+  {
+    id: "8",
+    name: "Soto Betawi",
+    ingredients: "500 gram daging sapi, potong dadu 1 liter santan",
+    spices:
+      "5 siung bawang putih 3 siung bawang merah 1 sdt ketumbar 1/2 sdt merica 1 sdt garam 1 sdm gula pasir 2 sdm minyak goreng",
+    steps:
+      "Rebus daging sapi hingga empuk. Angkat dan suwir-suwir dagingnya. Haluskan bawang putih, bawang merah, ketumbar, merica, garam, dan gula pasir. Tumis bumbu halus hingga harum. Masukkan santan. Aduk rata. Masukkan suwiran daging sapi. Aduk rata. Sajikan soto betawi dengan pelengkapnya.",
+    image: require("../assets/foods/8.png")
+  }
+];
